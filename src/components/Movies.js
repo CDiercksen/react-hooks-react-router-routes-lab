1e729e013be7306c1f792b6fbd2f8e02c57897b4
Christopher <christopher.diercksen@gmail.com>
2022-01-04 13:31:18 -0500

    commit tree 302e6af168504903f4a7fdf45ad69abf1fe9a6f3

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    <ul>{movies.map((movie) => (
+      <div key={movie.title}>
+        <h2>Name: {movie.title}</h2>
+        <h4>Time: {movie.time}</h4>
+        <h4>Genres:</h4>
+        <ul>{movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li> 
+        ))}
+        </ul>
+      </div>
+    ))}</ul>
+  </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,60 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  TextDecoration: "none",
+  color: "white",
+};
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background:"purple",
+        }}
+        >
+          Home
+        </NavLink>
+        <NavLink 
+        to="/Movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "purple",
+        }}
+        >
+          Movies
+        </NavLink>
+        <NavLink
+        to="/Directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "purple",
+        }}
+        >
+          Directors
+        </NavLink>
+        <NavLink
+        to="/Actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "purple",
+        }}
+        >
+          Actors
+        </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
